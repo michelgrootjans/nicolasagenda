15f21c75515802df8fde2ab324d53480ab377875
michel.grootjans <michel.grootjans@77de7ff6-a2ec-11de-af66-2b978f52db6e>
2010-09-27 08:31:55 +0000

    Cleanup

--- trunk/src/Agenda/Views/MaakTaakPresenter.cs
+++ trunk/src/Agenda/Views/MaakTaakPresenter.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System.Collections.Generic;
+using Agendas.Events;
+using Agendas.Infrastructure;
+
+namespace Agendas.Views
+{
+    internal class MaakTaakPresenter : IMaakTaakPresenter
+    {
+        private readonly IMaakTaakView view;
+
+        public MaakTaakPresenter(IMaakTaakView view)
+        {
+            this.view = view;
+        }
+
+        public void Initilaize()
+        {
+            view.Vakken = new List<string>{"FRA", "NED"};
+        }
+
+        public void CreateTaak()
+        {
+            using(var session = NHibernateProvider.CreateSession())
+            using(var transaction = session.BeginTransaction())
+            {
+                var dag = session.Query(new GetDayQuery(view.DateDue)).UniqueResult() ??
+                          DagFactory.CreateDag(view.DateDue);
+                dag.AddTaak(view.SelectedVak, view.Inhoud);
+                transaction.Commit();
+            }
+            EventAggregator.Raise(new TaakHasBeenCreated());
+        }
+    }
+}
